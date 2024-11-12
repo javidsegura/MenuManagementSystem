a6ece2db8106f1829621fea9497de4f8cad6768c
javidominguezsegura <javier.dominguez.segura@gmail.com>
2024-11-12 22:01:05 +0100

    added api call for pdf extraction

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/javierdominguezsegura/Programming/College/Sophomore/Databases/Final project/menu-management-system/.venv"
+set -gx VIRTUAL_ENV "/Users/javierdominguezsegura/Programming/College/Sophomore/Databases/Final project/MenuManagementSystem/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
